a7484e88319d69615e7992ee531f15851dcb6a32
Can UYUMAZ <58149556+tombensonnn@users.noreply.github.com>
2022-10-02 15:34:40 +0300

    code review first

---
+++ client/src/utility/index.js
@@ -0,0 +1,14 @@
+export const excerpt=(str,count) => {
+    if(str.length >count){
+        str = str.substring(0,count) + "..."
+    }
+    // return str.toLowerCase()
+    return str
+}
+
+export const excerptLower=(str,count) => {
+    if(str.length >count){
+        str = str.substring(0,count) + "..."
+    }
+    return str.toLowerCase()
+}
